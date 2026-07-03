2f223cc19c436bdfdb7f308fb153a5e137a39084
mekeirish <mmm1747lb@gmail.com>
2026-07-03 18:49:03 +0200

    Mastercode Sync

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'sog-forkeur-' + Date.now();
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./index.html','./manifest.json','./icon.png'])));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+const cacheName='sog-forkeur-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
